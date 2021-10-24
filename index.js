be168222d6751fc5c6ac802fd4c5e9377ef39d12
Ezequiel Becerra <ebecerra1984@gmail.com>
2021-10-23 22:56:23 -0300

    Maquetado productCart y footer

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.use("/static", express.static("./public"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/index.html"));
 });
+
+app.get("/productCart", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/productCart.html"));
+});
+
 app.listen(3000, () => {
   console.log("el servidor inicio...");
 });
