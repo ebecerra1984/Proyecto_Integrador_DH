8b0fd83ffe6c7cd90412093279e120c0484b0d55
ebecerra1984 <58277040+ebecerra1984@users.noreply.github.com>
2021-11-07 14:22:19 -0300

    cambio de nombre controller user

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const userCTRL= require('./controllers/user_controllers');
+const userCTRL = require("./controllers/user.controllers");
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use("/static", express.static("./public"));
-
 
 app.use("/login", userCTRL.login);
 app.use("/register", userCTRL.register);
