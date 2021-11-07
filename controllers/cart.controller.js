8c762472ba7a10333027b3acfff0e0f5b860c617
ebecerra1984 <58277040+ebecerra1984@users.noreply.github.com>
2021-11-07 14:30:24 -0300

    Cambio de metodo a crontroller

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,6 +1,6 @@
 const cartController = {
   cart: (req, res) => {
-    res.send("carrito");
+    res.render("cart");
   },
 };
 
--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const userCTRL = require("./controllers/user.controllers");
+const userCTRL = require("./controllers/user.controller");
+const cartController = require("./controllers/cart.controller");
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use("/static", express.static("./public"));
 
+app.use("/cart", cartController.cart);
 app.use("/login", userCTRL.login);
 app.use("/register", userCTRL.register);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/index.html"));
 });
-app.get("/cart", (req, res) => {
-  res.render("cart");
-});
+
 app.get("/prod-detail", (req, res) => {
   res.sendFile(path.join(__dirname, "views/productDetail.html"));
 });
