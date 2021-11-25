e4f9673017fd4ad7fa44821e58273ebbad705a42
Eduardo-Iza <edu70.i@gmail.com>
2021-11-25 10:17:55 -0300

    prodDetali modificado a vista dinámica, modifica productDetail.controller e index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const userCTRL = require("./controllers/user.controller");
-const cartController = require("./controllers/cart.controller");
-const productsDetails = require("./controllers/products.controller");
 const indexController = require("./controllers/index.controller");
+const cartController = require("./controllers/cart.controller");
+const userCTRL = require("./controllers/user.controller");
+const productDetailCTRL = require("./controllers/productDetail.controller");
 const productCrudCTRL = require("./controllers/productCrud.controller");
-const prodDetailCtrl = require("./controllers/productDetail.controller");
+//const productsDetails = require("./controllers/products.controller");
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
@@ -17,27 +17,29 @@ app.use("//", indexController.index);
 app.use("/cart", cartController.cart);
 app.use("/login", userCTRL.login);
 app.use("/register", userCTRL.register);
-app.use("/prod-detail", productsDetails.details);
+app.use("/prodDetail/:id", productDetailCTRL.detail);
 app.use("/productCrud", productCrudCTRL.productCrud);
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/index.html"));
-});
-
-app.get("/prod-detail", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/productDetail.html"));
-});
-
-/*ruta parametrizada de prueba Gerardo*/
-app.get("/productos/:id", function (req, res) {
-  let idProducto = req.params.id;
-  res.sendFile(
-    path.join(__dirname, "views/productDetail-" + idProducto + ".html")
-  );
-});
-/*fin ruta parametrizada de prueba para detalles de producto*/
-app.use("/prodDetail", prodDetailCtrl.load);
 
 app.listen(3000, () => {
   console.log("el servidor inicio...");
 });
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "views/index.html"));
+// });
+
+// app.get("/prod-detail", (req, res) => {
+//   res.sendFile(path.join(__dirname, "views/productDetail.html"));
+// });
+
+// /*ruta parametrizada de prueba Gerardo*/
+// app.get("/productos/:id", function (req, res) {
+//   let idProducto = req.params.id;
+//   res.sendFile(
+//     path.join(__dirname, "views/productDetail-" + idProducto + ".html")
+//   );
+// });
+// /*fin ruta parametrizada de prueba para detalles de producto*/
+
+// //app.use("/prodDetail", prodDetailCtrl.load);
