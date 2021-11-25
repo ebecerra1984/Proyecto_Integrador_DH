368e291719bc1d488cc536b99ea2e79af1b35e7a
Eduardo-Iza <edu70.i@gmail.com>
2021-11-24 21:48:38 -0300

    Index.ejs modificado a vista dinámica, modifica indexController, se corrige dato de products.json

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,6 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
 const indexController = {
   index: (req, res) => {
-    res.render("index");
+    const robotsFijos = products.filter(producto => producto.category == 'robot-fijo');
+		const robotsMoviles = products.filter(producto => producto.category == 'robot-movil');
+    res.render("index", { robotsFijos, robotsMoviles });
   },
 };
 
