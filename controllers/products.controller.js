8df8d78f65cf4654e74eab36c69a80ea73a00a1e
Ezequiel Becerra <ebecerra1984@gmail.com>
2021-11-25 13:48:00 -0300

    Cambios menores

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,24 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const productsFilePath = path.join(__dirname, '../data/products.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const productsFilePath = path.join(__dirname, "../data/products.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
 const productsCtrl = {
-   
-    detail: (req, res) => {
-      idProd = req.params.id;
-		  producto = products.find(function (product) {
-			return product.id == idProd;
-		});
-      res.render("prodDetail",{producto});
-    },
+  detail: (req, res) => {
+    idProd = req.params.id;
+    producto = products.find(function (product) {
+      return product.id == idProd;
+    });
+    res.render("prodDetail", { producto });
+  },
 
-    productCrud: (req, res)=>{
-      res.render('productCrud')
-    }
-  };
-  
-  module.exports = productsCtrl;
+  productCrud: (req, res) => {
+    res.render("productCrud");
+  },
+};
+
+module.exports = productsCtrl;
