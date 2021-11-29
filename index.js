3d0257b67ce9073aaea568f9dce08405ab45fddd
ebecerra1984 <58277040+ebecerra1984@users.noreply.github.com>
2021-11-29 00:09:11 -0300

    Agregado de multer en products controller

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ const productsRouter = require("./routes/products.routes");
 const indexController = require("./controllers/index.controller");
 const cartController = require("./controllers/cart.controller");
 const productsCTRL = require("./controllers/products.controller");
+const { use } = require("./routes/users.routes");
 
 // Seteo
 app.set("view engine", "ejs");
 app.set("views", "./views");
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded());
 app.use("/static", express.static("./public"));
 app.use(methodOverride("_method"));
 
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const image = multer({ dest: "/static/images" });
 
 const productsCTRL = require("../controllers/products.controller");
 
@@ -8,7 +10,7 @@ router.get("/prodFijos", productsCTRL.fijos);
 router.get("/prodMoviles", productsCTRL.moviles);
 router.get("/prodDetail/:id", productsCTRL.detail);
 
-router.post("/crear", productsCTRL.create);
+router.post("/crear", image.none(), productsCTRL.create);
 router.get("/prodCRUD", productsCTRL.prodCRUD);
 router.get("/edit/:id", productsCTRL.edit);
 router.put("/edit/:id", productsCTRL.update);
