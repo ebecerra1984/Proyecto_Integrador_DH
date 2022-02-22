efb8893a9953f80c4bc870483ad8dfd71a956e01
ebecerra1984 <58277040+ebecerra1984@users.noreply.github.com>
2022-02-21 23:17:22 -0300

    Cambios en asignacion de clases en validaciones

--- public/js/productEditValidate.js
+++ public/js/productEditValidate.js
@@ -1,67 +1,40 @@
-window.addEventListener("load", function () {
 let form = document.querySelector(".prodEditForm");
-console.log(form.elements);
 
 form.addEventListener("submit", function (event) {
-  //event.preventDefault();
-
   let errores = [];
 
-  let { name, description, category, newCategory,  price, discount } = form.elements;
-
+  let { name, description, category, newCategory, price, discount } =
+    form.elements;
   if (name.value.length < 5) {
     errores.push("El nombre del producto debe tener al menos 5 caracteres");
-    // name.classList.remove("is-valid-campo-input");
-    name.classList.add("is-invalid-campo-input");
-    console.log(name.value);
+    name.className = "is-invalid-campo-input";
   } else {
-    name.classList.remove("is-invalid-campo-input");
-    name.classList.add("is-valid-campo-input");
+    name.className = "is-valid-campo-input";
   }
   if (description.value.length < 20) {
     errores.push("La descripcion debe tener al menos 20 caracteres");
-    // description.classList.remove("is-valid-campo-input");
-    description.classList.add("is-invalid-campo-input");
+    description.className = "is-invalid-campo-input";
   } else {
-    description.classList.remove("is-invalid-campo-input");
-    description.classList.add("is-valid-campo-input");
+    description.className = "is-valid-campo-input";
   }
-  if (newCategory.value =="Seleccione nueva") {
+  if (newCategory.value == "Seleccione nueva") {
     errores.push("La nueva categoría no fue seleccionada");
-    // description.classList.remove("is-valid-campo-input");
-    newCategory.classList.add("is-invalid-newCategory");
+    newCategory.className = "is-invalid-newCategory";
   } else {
-    newCategory.classList.remove("is-invalid-newCategory");
-    newCategory.classList.add("is-valid-newCategory");
+    newCategory.className = "is-valid-newCategory";
   }
-
-
-  // if (image.value == "") {
-  //   errores.push(
-  //     "Debes seleccionar una imagen de producto (.jpg, .jpeg, .png, .gif)"
-  //   );
-  //   // image.classList.remove("is-valid-campo-input");
-  //   image.classList.add("is-invalid-campo-input");
-  // } else {
-  //   image.classList.remove("is-invalid-campo-input");
-  //   image.classList.add("is-valid-campo-input");
-  // }
 
   if (price.value == "") {
     errores.push("Debes ingresar un valor para el precio");
-    // price.classList.remove("is-valid-campo-input");
-    price.classList.add("is-invalid-campo-input");
+    price.className = "is-invalid-campo-input";
   } else {
-    price.classList.remove("is-invalid-campo-input");
-    price.classList.add("is-valid-campo-input");
+    price.className = "is-valid-campo-input";
   }
   if (discount.value == "") {
     errores.push("Debes ingresar un valor de descuento");
-    // discount.classList.remove("is-valid-campo-input");
-    discount.classList.add("is-invalid-campo-input");
+    discount.className = "is-invalid-campo-input";
   } else {
-    discount.classList.remove("is-invalid-campo-input");
-    discount.classList.add("is-valid-campo-input");
+    discount.className = "is-valid-campo-input";
   }
 
   let erroresUL = document.getElementById("erroresFront");
@@ -77,5 +50,4 @@ form.addEventListener("submit", function (event) {
   } else {
     return true;
   }
-});
 });
--- public/js/productValidate.js
+++ public/js/productValidate.js
@@ -1,58 +1,48 @@
-// window.addEventListener("load", function () {
 let form = document.querySelector(".prodForm");
-console.log(form.elements);
 
 form.addEventListener("submit", function (event) {
-  //   event.preventDefault();
-
   let errores = [];
 
   let { nombre, descripcion, imagen, precio, descuento } = form.elements;
 
   if (nombre.value.length < 5) {
     errores.push("El nombre del producto debe tener al menos 5 caracteres");
-    // nombre.classList.remove("is-valid-campo-input");
-    nombre.classList.add("is-invalid-campo-input");
-    console.log(nombre.value);
+    nombre.className = "is-invalid-campo-input";
   } else {
     nombre.classList.remove("is-invalid-campo-input");
-    nombre.classList.add("is-valid-campo-input");
+    nombre.className = "is-valid-campo-input";
   }
   if (descripcion.value.length < 20) {
     errores.push("La descripcion debe tener al menos 20 caracteres");
-    // nombre.classList.remove("is-valid-campo-input");
-    descripcion.classList.add("is-invalid-campo-input");
+    descripcion.className = "is-invalid-campo-input";
   } else {
     descripcion.classList.remove("is-invalid-campo-input");
-    descripcion.classList.add("is-valid-campo-input");
+    descripcion.className = "is-valid-campo-input";
   }
 
   if (imagen.value == "") {
     errores.push(
       "Debes seleccionar una imagen de producto (.jpg, .jpeg, .png, .gif)"
     );
-    // nombre.classList.remove("is-valid-campo-input");
-    imagen.classList.add("is-invalid-campo-input");
+    imagen.className = "is-invalid-campo-input";
   } else {
     imagen.classList.remove("is-invalid-campo-input");
-    imagen.classList.add("is-valid-campo-input");
+    imagen.className = "is-valid-campo-input";
   }
 
   if (precio.value == "") {
     errores.push("Debes ingresar un valor para el precio");
-    // nombre.classList.remove("is-valid-campo-input");
-    precio.classList.add("is-invalid-campo-input");
+    precio.className = "is-invalid-campo-input";
   } else {
     precio.classList.remove("is-invalid-campo-input");
-    precio.classList.add("is-valid-campo-input");
+    precio.className = "is-valid-campo-input";
   }
   if (descuento.value == "") {
     errores.push("Debes ingresar un valor de descuento");
-    // nombre.classList.remove("is-valid-campo-input");
-    descuento.classList.add("is-invalid-campo-input");
+    descuento.className = "is-invalid-campo-input";
   } else {
     descuento.classList.remove("is-invalid-campo-input");
-    descuento.classList.add("is-valid-campo-input");
+    descuento.className = "is-valid-campo-input";
   }
 
   let erroresUL = document.getElementById("erroresFront");
@@ -69,4 +59,3 @@ form.addEventListener("submit", function (event) {
     return true;
   }
 });
-// });
