ac726dd132370c69f4a1f10ba057ff42bc291c43
mmschiavon <nfo@iacon.com.ar>
2022-02-22 23:35:08 -0300

    actualizacion api product controller 2

--- controllers/api/productsAPIController.js
+++ controllers/api/productsAPIController.js
@@ -1,29 +1,23 @@
-const path = require("path");
+// const path = require("path");
 const db = require("../../database/models");
 
-//Aqui tienen otra forma de llamar a cada uno de los modelos
-const Movies = db.Movie;
-const Genres = db.Genre;
-const Actors = db.Actor;
-
-const moviesAPIController = {
+const productsAPIController = {
   list: (req, res) => {
-    db.Movie.findAll({ include: ["genre"] })
-    .then((movies) => {
+    db.Product.findAll({ include: ["ProductCategory"] }).then((products) => {
       let respuesta = {
         meta: {
           status: 200,
-          total: movies.length,
-          url: "api/movies",
+          total: products.length,
+          url: "api/products",
         },
 
-        data: movies.map((movie) => {
+        data: products.map((product) => {
           return {
-            title: movie.title,
-            length: movie.length,
-            rating: movie.rating,
-            awards: movie.awards,
-            genre: movie.genre.name,
+            id: product.id,
+            name: product.nombre,
+            description: product.descripcion,
+            category: product.ProductCategory.nombre,
+            //URL:?????????????????
           };
         }),
       };
@@ -32,143 +26,20 @@ const moviesAPIController = {
   },
 
   detail: (req, res) => {
-    db.Movie.findByPk(req.params.id, {
-      include: ["genre"],
-    }).then((movie) => {
+    db.Product.findByPk(req.params.id, {
+      include: ["ProductCategory"],
+    }).then((product) => {
       let respuesta = {
         meta: {
           status: 200,
-          total: movie.length,
-          url: "/api/movie/:id",
+          total: product.length,
+          url: "/api/product/:id",
         },
-        data: movie,
+        data: product,
       };
       res.json(respuesta);
     });
   },
-  recomended: (req, res) => {
-    db.Movie.findAll({
-      include: ["genre"],
-      where: {
-        rating: { [db.Sequelize.Op.gte]: req.params.rating },
-      },
-      order: [["rating", "DESC"]],
-    })
-      .then((movies) => {
-        let respuesta = {
-          meta: {
-            status: 200,
-            total: movies.length,
-            url: "api/movies/recomended/:rating",
-          },
-          data: movies,
-        };
-        res.json(respuesta);
-      })
-      .catch((error) => console.log(error));
-  },
-  create: (req, res) => {
-    Movies.create({
-      title: req.body.title,
-      rating: req.body.rating,
-      awards: req.body.awards,
-      release_date: req.body.release_date,
-      length: req.body.length,
-      genre_id: req.body.genre_id,
-    })
-      .then((confirm) => {
-        let respuesta;
-        if (confirm) {
-          respuesta = {
-            meta: {
-              status: 200,
-              total: confirm.length,
-              url: "api/movies/create",
-            },
-            data: confirm,
-          };
-        } else {
-          respuesta = {
-            meta: {
-              status: 200,
-              total: confirm.length,
-              url: "api/movies/create",
-            },
-            data: confirm,
-          };
-        }
-        res.json(respuesta);
-      })
-      .catch((error) => res.send(error));
-  },
-  update: (req, res) => {
-    let movieId = req.params.id;
-    Movies.update(
-      {
-        title: req.body.title,
-        rating: req.body.rating,
-        awards: req.body.awards,
-        release_date: req.body.release_date,
-        length: req.body.length,
-        genre_id: req.body.genre_id,
-      },
-      {
-        where: { id: movieId },
-      }
-    )
-      .then((confirm) => {
-        let respuesta;
-        if (confirm) {
-          respuesta = {
-            meta: {
-              status: 200,
-              total: confirm.length,
-              url: "api/movies/update/:id",
-            },
-            data: confirm,
-          };
-        } else {
-          respuesta = {
-            meta: {
-              status: 204,
-              total: confirm.length,
-              url: "api/movies/update/:id",
-            },
-            data: confirm,
-          };
-        }
-        res.json(respuesta);
-      })
-      .catch((error) => res.send(error));
-  },
-  destroy: (req, res) => {
-    let movieId = req.params.id;
-    Movies.destroy({ where: { id: movieId }, force: true }) // force: true es para asegurar que se ejecute la acción
-      .then((confirm) => {
-        let respuesta;
-        if (confirm) {
-          respuesta = {
-            meta: {
-              status: 200,
-              total: confirm.length,
-              url: "api/movies/destroy/:id",
-            },
-            data: confirm,
-          };
-        } else {
-          respuesta = {
-            meta: {
-              status: 204,
-              total: confirm.length,
-              url: "api/movies/destroy/:id",
-            },
-            data: confirm,
-          };
-        }
-        res.json(respuesta);
-      })
-      .catch((error) => res.send(error));
-  },
 };
 
-module.exports = moviesAPIController;
+module.exports = productsAPIController;
